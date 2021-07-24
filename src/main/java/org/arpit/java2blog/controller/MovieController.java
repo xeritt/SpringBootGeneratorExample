97683e726504ae5b884b14427e00746ca9abbdb6
Nick Chi <xeritt@gmail.com>
2021-07-24 16:41:57 +0700

    Исправил mapping иначе пропадала сонсоль h2

--- src/main/java/org/arpit/java2blog/controller/MovieController.java
+++ src/main/java/org/arpit/java2blog/controller/MovieController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
 
+//@RequestMapping("/movies")
 @RestController
-@RequestMapping("/movies")
 @Tag(name = "Movies Service", description = "Пример реализации REST сервиса")
 public class MovieController {
 
@@ -24,12 +24,12 @@ public class MovieController {
     MovieService movieService;
 
     @Operation(summary = "Получение списка сущностей")
-    @GetMapping(produces = "application/json")
+    @GetMapping(value = "/movies", produces = "application/json")
     private List<Movie> getMovies() {
         return movieService.getAllMovies();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/movies/{id}")
     @Operation(summary = "Получение сущности по id")
     private Movie getMovie(@PathVariable("id") int id) {
         return movieService.getMovieById(id);
@@ -42,14 +42,14 @@ public class MovieController {
             @ApiResponse(responseCode = "404", description = "строка не найдена", content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "500", description = "внутренняя ошибка сервера")
     })
-    @DeleteMapping("{id}")
+    @DeleteMapping("/movies/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     private void deleteMovie(@PathVariable("id") int id) {
         movieService.delete(id);
     }
 
     @Operation(summary = "Изменение сущности по id")
-    @PostMapping(value = "/save", produces = "application/json")
+    @PostMapping(value = "/movies/save", produces = "application/json")
     private int saveMovie(@RequestBody Movie movie) {
         movieService.saveOrUpdate(movie);
         return movie.getId();
@@ -61,7 +61,7 @@ public class MovieController {
             @ApiResponse(responseCode = "400", description = "неверный формат запроса"),
             @ApiResponse(responseCode = "500", description = "внутренняя ошибка сервера")
     })
-    @PostMapping(value = "/new", produces = "application/json")
+    @PostMapping(value = "/movies/new", produces = "application/json")
     @ResponseBody
     public Movie createMovie(@Validated @NotBlank @RequestBody Movie movie) {
         return movieService.createMovie(movie);
